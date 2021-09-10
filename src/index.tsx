8007d9ee1fdcb7d224f07ed508503cb139e65be6
Simon Bragg <simon.bragg+github@gmail.com>
2021-09-10 15:00:36 +1000

    Get back to working

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import '@blueprintjs/core/lib/css/blueprint.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
-import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
